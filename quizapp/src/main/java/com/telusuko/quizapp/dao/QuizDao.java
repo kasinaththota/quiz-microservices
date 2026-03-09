cdafd2c464661c1de5ab016f8fa7c79991214fba
Kasinath Thota <kasithota02@gmail.com>
2026-03-09 18:53:40 +0530

    deleted quizapp

--- quizapp/src/main/java/com/telusuko/quizapp/dao/QuizDao.java
+++
@@ -1,9 +0,0 @@
-package com.telusuko.quizapp.dao;
-
-import com.telusuko.quizapp.model.Quiz;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface QuizDao extends JpaRepository<Quiz,Integer> {
-}
